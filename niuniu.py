1002a05f50a88b6afff8ed7aa982c472129713e1
FakeYoumo <fakeyoumo@tutamail.com>
2024-05-10 08:53:24 +0800

    丰富提示词

--- niuniu.py
+++ niuniu.py
@@ -45,7 +45,22 @@ async def main():
                 elif mention.body == f"u/{bot_name} 查成分":
                     comment = await reddit.comment(mention.parent_id)
                     need_judged_person = comment.author.name
-                    prompt = "你的名字叫牛牛，你生活在reddit，你可以根据用户的历史发言来判断用户的成分，包括国籍，祖籍，省市县，现在生活位置，学校，性别，性取向，爱好，职业，宗教信仰，政治倾向等等各个方面的数据，用户发的post如下：\n"
+                    prompt = '''
+你的名字叫牛牛，你生活在reddit，你是一位拥有多年在线简介和文本分析经验的专家。
+你以分析思维工作，并尝试尽可能准确地回答问题，你可以根据用户的历史发言来判断用户的成分，包括国籍，祖籍，省市县，现在生活位置，学校，性别，性取向，爱好，职业，感情状况，富裕程度，宗教信仰，政治倾向等等
+各个方面的数据。
+这是几个可能用到的标签以及解释，你在总结时可以使用，
+    - 太监：r/China_irl 用户
+    - 小瘤：留学生
+    - 姨粉：刘仲敬（阿姨）的粉丝
+    - 真太监：r/real_china_irl 用户
+    - 浪友：喜欢口嗨图纸
+    - 神友：前神奈川冲浪里的用户，被艳红橄榄之后流窜到红迪建立r/chonglangTV
+    - 粉红：非中共体制利益既得者，但无脑支持中共，没有独立思考能力，完全支持中共宣传话术并自发维护这个秩序
+    - 红老嗨：中共利益既得者，真正的赵家人，在中国拥有社会地位与权力，财富来源于人矿
+    - 默友/抹油：原r/Youmo 用户，现在r/Youmo已被红迪橄榄
+用户发的post如下：
+'''
                     redditor = await reddit.redditor(need_judged_person)
                     async for post in redditor.submissions.new(limit=15):
                         title = post.title
